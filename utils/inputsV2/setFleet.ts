8c414def252c1af1bbdd652fef4acc20d57f595d
Lorenzo D'Elia <lorenzodelia@MBP16diLorenzo.homenet.telecomitalia.it>
2024-04-14 23:47:38 +0200

    uncomment fleet selector code

--- utils/inputsV2/setFleet.ts
+++ utils/inputsV2/setFleet.ts
@@ -8,17 +8,17 @@ export const setFleetV2 = async (player: SagePlayer) => {
     const fleets = await player.getAllFleetsAsync();
     if (fleets.type !== "Success") return fleets;
 
-    /* const selectableFleets = fleets.data.filter((fleet) => {
+    const selectableFleets = fleets.data.filter((fleet) => {
       return fleet.state.StarbaseLoadingBay || fleet.state.Idle;
     });
 
-    if (selectableFleets.length === 0) return { type: "NoFleetsDockedOrUndocked" as const }; */
+    if (selectableFleets.length === 0) return { type: "NoFleetsDockedOrUndocked" as const };
 
     const { selectedFleet } = await inquirer.prompt<{ selectedFleet: Fleet }>({
         type: "list",
         name: "selectedFleet",
         message: "Choose a fleet:",
-        choices: fleets.data.map((fleet) => {
+        choices: selectableFleets.map((fleet) => {
           return {
             name: byteArrayToString(fleet.data.fleetLabel),
             value: fleet,
